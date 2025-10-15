e3aff514b2e5cfe25c5344aadd7e2ffe3f66f972
Guillaume BAILLEUL <guillaume.bailleul@gmail.com>
2025-10-15 21:16:36 +0200

    add dializyer to test env

--- mix.exs
+++ mix.exs
@@ -67,7 +67,7 @@ defmodule Quant.Explorer.MixProject do
       {:ex_doc, "~> 0.31", only: :dev, runtime: false},
       {:git_hooks, "0.8.0", only: :dev, runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:bypass, "~> 2.1", only: :test},
       {:excoveralls, "~> 0.18", only: :test},
       {:pythonx, "~> 0.2", only: :test}
